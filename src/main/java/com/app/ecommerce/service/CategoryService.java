4a9b92400843e1fc95638421e89b1d8c9189ddbc
Shubham Wagh <shubhamwagh2544@gmail.com>
2023-10-09 00:11:25 +0530

    service class

---
+++ src/main/java/com/app/ecommerce/service/CategoryService.java
@@ -0,0 +1,37 @@
+package com.app.ecommerce.service;
+
+import com.app.ecommerce.model.Category;
+import com.app.ecommerce.model.CategoryRequest;
+import com.app.ecommerce.repository.CategoryDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+    @Autowired
+    @Qualifier("jpa")
+    private CategoryDao categoryDao;
+
+    public String createCategory(CategoryRequest categoryRequest) {
+        return categoryDao.createCategory(categoryRequest);
+    }
+
+    public List<Category> getAllCategories() {
+        return categoryDao.getAllCategories();
+    }
+
+    public Category getCategory(Long categoryId) {
+        return categoryDao.getCategory(categoryId);
+    }
+
+    public String updateCategory(Long categoryId, CategoryRequest categoryRequest) {
+        return categoryDao.updateCategory(categoryId, categoryRequest);
+    }
+
+    public String deleteCategory(Long categoryId) {
+        return categoryDao.deleteCategory(categoryId);
+    }
+}
